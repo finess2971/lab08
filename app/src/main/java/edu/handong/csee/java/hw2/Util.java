b514a0a72a731f12dbe80be506f63a938f6da675
kihon <kihon@LAPTOP-57ACHA37>
2021-05-06 16:51:33 +0900

    Util

---
+++ app/src/main/java/edu/handong/csee/java/hw2/Util.java
@@ -0,0 +1,16 @@
+package edu.handong.csee.java.hw2;
+
+/**
+ * get args and if they not uppercase, change them uppercase
+ */
+public class Util {
+
+    /**
+     * @param stringValue args 
+     * @return Uppercase args
+     */
+    static String getUppercaseString(String stringValue) {
+        return stringValue.toUpperCase();
+    }
+    
+}
